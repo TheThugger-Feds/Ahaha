e0e0ff84be592c67d79acf551c4d444de5a20605
TheThugger-Feds <etogagames@gmail.com>
2026-03-10 18:13:41 -0500

    Update AhahaBurg_Main.lua

--- AhahaBurg_Main.lua
+++ AhahaBurg_Main.lua
@@ -1,81 +1,67 @@
-local Rayfield = loadstring(game:HttpGet("https://raw.githubusercontent.com/shlexware/Rayfield/main/source"))()
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
-local TaxiFarmURL = "https://raw.githubusercontent.com/TheThugger-Feds/Ahaha/refs/heads/main/TaxiAutoFarm.lua"
+local TaxiFarmURL = "https://raw.githubusercontent.com/TheThugger-Feds/Ahaha/main/TaxiAutoFarm.lua"
 local HWID = game:GetService("RbxAnalyticsService"):GetClientId()
 local KeyLink = "https://new.pandadevelopment.net/getkey/ahahaburg?hwid=" .. HWID
 
 local Window = Rayfield:CreateWindow({
-Name = "AhahaBurg",
-LoadingTitle = "Authenticating...",
-LoadingSubtitle = "by @ETOGA61",
-ConfigurationSaving = {
-Enabled = true,
-FolderName = "AhahaBurg_Configs",
-FileName = "MainUI"
-},
-KeySystem = true,
-KeySettings = {
-Title = "AhahaBurg | Key System",
-Subtitle = "Press Enter to copy key link",
-Note = "Press the button or Enter to copy the key link",
-FileName = "AhahaBurgKey",
-SaveKey = true,
-GrabKeyFromSite = false,
-Key = {"Ahaha_Success"},
-Actions = {
-[1] = {
-Name = "Copy Key Link",
-Callback = function()
-setclipboard(KeyLink)
-Rayfield:Notify({
-Title = "Link Copied",
-Content = "Key link copied to clipboard",
-Duration = 4
-})
-end
-}
-}
-}
-})
-
-local KeyTab = Window:CreateTab("Key", 4483362458)
-
-KeyTab:CreateButton({
-Name = "Copy Key Link",
-Callback = function()
-setclipboard(KeyLink)
-Rayfield:Notify({
-Title = "Link Copied",
-Content = "Key link copied to clipboard",
-Duration = 4
-})
-end
+    Name = "AhahaBurg",
+    LoadingTitle = "Authenticating...",
+    LoadingSubtitle = "by @ETOGA61",
+    ConfigurationSaving = {
+        Enabled = true,
+        FolderName = "AhahaBurg_Configs",
+        FileName = "MainUI"
+    },
+    KeySystem = true,
+    KeySettings = {
+        Title = "AhahaBurg | Key System",
+        Subtitle = "Enter the key to continue",
+        Note = "Copy the link and get your key!",
+        FileName = "AhahaBurgKey",
+        SaveKey = true,
+        GrabKeyFromSite = false,
+        Key = {"Ahaha_Success"}, -- Ensure your Panda key matches this
+        Actions = {
+            [1] = {
+                Name = "Copy Key Link",
+                Callback = function()
+                    setclipboard(KeyLink)
+                    Rayfield:Notify({
+                        Title = "Link Copied",
+                        Content = "Panda link copied to clipboard!",
+                        Duration = 5
+                    })
+                end
+            }
+        }
+    }
 })
 
 local FarmTab = Window:CreateTab("Autofarm", 4483362458)
 
 FarmTab:CreateToggle({
-Name = "Enable Taxi Farm",
-CurrentValue = false,
-Flag = "TaxiToggle",
-Callback = function(Value)
-_G.TaxiToggle = Value
-if Value and not _G.TaxiBotInitiated then
-loadstring(game:HttpGet(TaxiFarmURL))()
-_G.TaxiBotInitiated = true
-end
-end
+    Name = "Enable Taxi Farm",
+    CurrentValue = false,
+    Flag = "TaxiToggle",
+    Callback = function(Value)
+        _G.TaxiToggle = Value
+        if Value and not _G.TaxiBotInitiated then
+            _G.TaxiBotInitiated = true
+            loadstring(game:HttpGet(TaxiFarmURL))()
+        end
+    end
 })
 
 FarmTab:CreateSlider({
-Name = "Farm Speed",
-Range = {16, 100},
-Increment = 1,
-CurrentValue = 36,
-Flag = "TaxiSpeed",
-Callback = function(Value)
-_G.TaxiFarmSpeed = Value
-end
+    Name = "Farm Speed",
+    Range = {16, 100},
+    Increment = 1,
+    CurrentValue = 36,
+    Flag = "TaxiSpeed",
+    Callback = function(Value)
+        _G.TaxiFarmSpeed = Value
+    end
 })
 
 Rayfield:LoadConfiguration()
